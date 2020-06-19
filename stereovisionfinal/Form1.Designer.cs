071c90eecc90754d3b63e47f9a783a15ba14575b
emirmsn <mehmetemirmersin10@gmail.com>
2020-06-19 14:20:12 +0300

    Initialize

---
+++ stereovisionfinal/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace stereovisionfinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Location = new System.Drawing.Point(44, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 247);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox2.Location = new System.Drawing.Point(359, 31);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(263, 247);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(441, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Detect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox3.Location = new System.Drawing.Point(673, 31);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(278, 247);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(151, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(477, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 574);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ stereovisionfinal/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace stereovisionfinal
+{
+    public partial class Form1 : Form
+    {
+        Capture capture1;
+        Capture capture2;
+        Rectangle faceL;
+        float xr, yr, xl, yl,y,x,F,D, Z;
+        Rectangle faceR;
+        CascadeClassifier classifierFace = new CascadeClassifier("C:/Users/Emir/Downloads/stereovisionfinal/stereovisionfinal/stereovisionfinal/haarcascade_frontalface_default.xml");
+       
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (capture1==null)
+            {
+                capture1 = new Capture(0);
+            }
+            capture1.ImageGrabbed += timer1_Tick;
+            capture1.Start();
+            if (capture2 == null)
+            {
+                capture2 = new Capture(1);
+            }
+            capture2.ImageGrabbed += timer1_Tick;
+            capture2.Start();
+            xr = (faceL.Y - faceR.Y);
+            yr = (faceL.X - faceR.X);
+            F = 800;
+            D = 1500;
+            Z = (F * D) / (xr * yr);
+
+            
+            
+
+            label1.Text = "Distance is:" + Z + "Y is: " + xr + "X is: " + yr;
+
+           
+
+            
+        }
+
+        
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                Mat Frame1 = new Mat();
+                capture1.Retrieve(Frame1);
+                
+                var ImageFrame1 = Frame1.ToImage<Bgr, Byte>();
+                if (ImageFrame1 != null)
+                {
+                    var grayFrame = ImageFrame1.Convert<Gray, byte>();
+                    Rectangle[] faces = classifierFace.DetectMultiScale(grayFrame, 1.3, 5);
+                    foreach (var face in faces)
+                    {
+                        ImageFrame1.Draw(face, new Bgr(Color.BurlyWood), 3);
+                        faceL = face;
+                    }
+                    
+                    pictureBox1.Image = ImageFrame1.ToBitmap();
+                   
+                    var grayFace = ImageFrame1.Convert<Gray, byte>();
+                    pictureBox3.Image = grayFace.ToBitmap();
+                    
+
+                }
+                Mat Frame2 = new Mat();
+                capture2.Retrieve(Frame2);
+                
+                var ImageFrame2 = Frame2.ToImage<Bgr, Byte>();
+                if (ImageFrame2 != null)
+                {
+                    var grayFrame = ImageFrame2.Convert<Gray, byte>();
+                    var faces = classifierFace.DetectMultiScale(grayFrame, 1.3, 5);
+
+
+
+
+                    foreach (var face in faces)
+                    {
+                        ImageFrame2.Draw(face, new Bgr(Color.BurlyWood), 3);
+                        faceR = face;
+                    }
+                    
+                     
+                    
+                    
+
+                    pictureBox2.Image = ImageFrame2.ToBitmap();
+                
+                }
+                
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
